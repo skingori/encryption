2dcedd364fd6a1ee35c5f8f312af66e8ec576e4a
skingori <infor.samson@gmail.com>
2017-07-30 20:14:20 +0300

    ds

--- index.php
+++ index.php
@@ -86,6 +86,8 @@ if (isset($_POST['login'])){
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
     <link rel="stylesheet" href="assets/font-awesome/4.5.0/css/font-awesome.min.css" />
 
+
+    <link rel="stylesheet" href="assets/css/background.css" />
     <!-- text fonts -->
     <link rel="stylesheet" href="assets/css/fonts.googleapis.com.css" />
 
